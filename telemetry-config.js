33f14a7ddd36e9cfd2d65f44dc7fa5eab35c913a
Joe Kristiansen <kristiansenjf@proton.me>
2026-08-03 00:53:03 +0300

    Enable anonymous telemetry collector

--- telemetry-config.js
+++ telemetry-config.js
@@ -1,3 +1,3 @@
 // Set this to the deployed Worker's /collect URL after following
 // telemetry-worker/README.md. Leaving it blank disables telemetry completely.
-export const TELEMETRY_ENDPOINT = "";
+export const TELEMETRY_ENDPOINT = "https://ashes-of-babel-telemetry.twistyroads.workers.dev/collect";
